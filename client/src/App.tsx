5321dddca22e3a7b23718e5678d23c580ecc8939
Thomas CASTELLY <thomas@castelly.fr>
2023-02-03 15:53:22 +0100

    workaround using computed instead of ref

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,13 @@
-import { computed, defineComponent, ref, watch } from 'vue';
+import { computed, defineComponent} from 'vue';
 import { useQuery } from '@vue/apollo-composable';
 import { GET_YEAR, HELLO } from './queries';
 
 export default defineComponent({
   name: 'MyApp',
   setup() {
-    const yr = ref();
-
     const { result: _yr } = useQuery(GET_YEAR);
+
+    const yr = computed(() => _yr.value?.getYear);
 
     const { result: _msg } = useQuery(HELLO, () => ({
       yr: yr.value,
@@ -17,15 +17,6 @@ export default defineComponent({
     }));
 
     const msg = computed(() => _msg.value?.hello || '');
-
-    watch(
-      () => _yr.value,
-      (v) => {
-        if (v) {
-          yr.value = v.getYear;
-        }
-      },
-    );
 
     return () => <span v-text={msg.value} />;
   },
